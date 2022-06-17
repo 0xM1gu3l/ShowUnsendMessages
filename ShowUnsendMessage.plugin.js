4570f43b13b80bfe93df1e2c720d9af319ba0ed2
M1gu3l0001 <miguelpicolocaparroz@gmail.com>
2022-06-17 18:05:26 -0400

    Learning BetterDiscord...

--- ShowUnsendMessage.plugin.js
+++ ShowUnsendMessage.plugin.js
@@ -1,57 +1,21 @@
 /**
- * @name ShowUnsendMessage
+ * @name ShowUnsendMessages
  * @author M1gu3l0001
- * @authorId 427292531160776706
- * @version 1.0.0
- * @description Get access to the message of the user before he/she sends it.
+ * @description Be able to see messages before they are sen't to the server.
+ * @version 0.1
+ * @authorLink https://twitter.com/Migu3l0001
+ * @website https://github.com/M1gu3l0001/
+ * @source https://github.com/M1gu3l0001/ShowUnsendMessages
+ * @updateUrl https://raw.githubusercontent.com/M1gu3l0001/ShowUnsendMessages/master/ShowUnsendMessage.plugin.js
  */
 
- module.exports = (_ => {
-	const config = {
-		"info": {
-			"name": "ShowUnsendMessage",
-			"author": "M1gu3l0001",
-			"version": "1.0.0",
-			"description": "Get access to the message of the user before he/she sends it."
-		}
-	};
-    return !window.BDFDB_Global || (!window.BDFDB_Global.loaded && !window.BDFDB_Global.started) ? class {
-		getName () {return config.info.name;}
-		getAuthor () {return config.info.author;}
-		getVersion () {return config.info.version;}
-		getDescription () {return `The Library Plugin needed for ${config.info.name} is missing. Open the Plugin Settings to download it. \n\n${config.info.description}`;}
-		
-		downloadLibrary () {
-			require("request").get("https://mwittrien.github.io/BetterDiscordAddons/Library/0BDFDB.plugin.js", (e, r, b) => {
-				if (!e && b && r.statusCode == 200) require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0BDFDB.plugin.js"), b, _ => BdApi.showToast("Finished downloading BDFDB Library", {type: "success"}));
-				else BdApi.alert("Error", "Could not download BDFDB Library Plugin. Try again later or download it manually from GitHub: https://mwittrien.github.io/downloader/?library");
-			});
-		}
-		
-		load () {
-			if (!window.BDFDB_Global || !Array.isArray(window.BDFDB_Global.pluginQueue)) window.BDFDB_Global = Object.assign({}, window.BDFDB_Global, {pluginQueue: []});
-			if (!window.BDFDB_Global.downloadModal) {
-				window.BDFDB_Global.downloadModal = true;
-				BdApi.showConfirmationModal("Library Missing", `The Library Plugin needed for ${config.info.name} is missing. Please click "Download Now" to install it.`, {
-					confirmText: "Download Now",
-					cancelText: "Cancel",
-					onCancel: _ => {delete window.BDFDB_Global.downloadModal;},
-					onConfirm: _ => {
-						delete window.BDFDB_Global.downloadModal;
-						this.downloadLibrary();
-					}
-				});
-			}
-			if (!window.BDFDB_Global.pluginQueue.includes(config.info.name)) window.BDFDB_Global.pluginQueue.push(config.info.name);
-		}
-		start () {this.load();}
-		stop () {}
-		getSettingsPanel () {
-			let template = document.createElement("template");
-			template.innerHTML = `<div style="color: var(--header-primary); font-size: 16px; font-weight: 300; white-space: pre; line-height: 22px;">The Library Plugin needed for ${config.info.name} is missing.\nPlease click <a style="font-weight: 500;">Download Now</a> to install it.</div>`;
-			template.content.firstElementChild.querySelector("a").addEventListener("click", this.downloadLibrary);
-			return template.content.firstElementChild;
-		}
-    }
-})(window.BDFDB_Global.PluginUtils.buildPlugin(config));
-})();
+ module.exports = class ShowUnsendMessages {
+    load() {} // Optional function. Called when the plugin is loaded in to memory
+
+    start() {
+		showToast("Hello! ShowUnsendMessages entered on the party!", {type:"success", icon: true});
+	} // Required function. Called when the plugin is activated (including after reloads)
+    stop() {} // Required function. Called when the plugin is deactivated
+
+    observer(changes) {} // Optional function. Observer for the `document`. Better documentation than I can provide is found here: <https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver>
+}
